c4f33b9c5893861db6bb70d83db2174db3c38a96
chapman-darlene <designgallerynh@gmail.com>
2021-09-24 23:21:52 -0400

    changes to port number app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
@@ -21,4 +22,5 @@ app.use((req, res, next) => {
   res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
-app.listen(3000);
+app.listen(PORT);
+
