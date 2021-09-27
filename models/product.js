3ec9fba1c4d7f4ae8ec94ffe39ecb32fc2e6cc1a
chapman-darlene <designgallerynh@gmail.com>
2021-09-27 18:23:34 -0400

    creating MVC and start of shopping cart

---
+++ models/product.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(
+  path.dirname(require.main.filename),
+  'data',
+  'products.json'
+);
+
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
+
+module.exports = class Product {
+  constructor(title, imageUrl, description, price, rating) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.price = price;
+    this.description = description;
+    this.rating = rating;
+  }
+
+  save() {
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
+  }
+
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+};
--- routes/admin.js
+++ routes/admin.js
@@ -1,54 +1,15 @@
 const path = require('path');
-
 const express = require('express');
-
-const rootDir = require('../util/path');
+const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
-const products = [];
-
 // /admin/add-product => GET
-router.get('/add-product', (req, res, next) => {
-  res.render('add-product', {
-    pageTitle: 'Add Product',
-    path: '/admin/add-product',
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true
-  });
-});
+router.get('/add-product', adminController.getAddProduct);
+// /admin/products => GET
+router.get('/products', adminController.getProducts);
 
 // /admin/add-product => POST
-router.post('/add-product', (req, res, next) => {
-  products.push({
-    title: req.body.title,
-    price: req.body.price,
-    description: req.body.description,
-    rating: req.body.rating
-    });
-  res.redirect('/');
-});
+router.post('/add-product', adminController.postAddProduct);
 
-
-// /admin/remove-product => GET
-// router.get('/remove-product', (req, res, next) => {
-//   res.render('remove-product', {
-//     pageTitle: 'Remove Product',
-//     path: '/admin/remove-product',
-//     formsCSS: true,
-//     productCSS: true,
-//     activeRemoveProduct: true
-//   });
-// });
-
-
-router.post('/remove-product', (req, res, next) => {
-
-  products.splice({ title: req.body.title }, 1);
-  res.redirect('/');
-});
-
-
-exports.routes = router;
-exports.products = products;
+module.exports = router;
